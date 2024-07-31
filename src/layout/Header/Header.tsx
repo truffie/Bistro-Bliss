98a7be5a899d0c3bab56e6b3ae46deda6c34bf4b
Truffie <trufanov.vl2013@gmail.com>
2024-07-31 16:47:53 +0300

    feat: add layout header component

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+import styles from './Header.module.scss';
+import Link from 'next/link';
+import { Button } from '@components/Button/Button';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.contact}`}>
+        <div className={styles.wrapper}>
+          <div className={styles.communication}>
+            <Link href="">
+              <p>(414) 857 - 0107</p>
+            </Link>
+            <Link href="">
+              <p>yummy@bistrobliss</p>
+            </Link>
+          </div>
+          <div className={styles.socialMedia}>
+            <Link href="#">
+              <div>X</div>
+            </Link>
+            <Link href="#">
+              <div>Facebook</div>
+            </Link>
+            <Link href="#">
+              <div>Instagram</div>
+            </Link>
+            <Link href="#">
+              <div>Github</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.wrapper}>
+          <div className={styles.logo}>
+            <div></div>
+            <p>Bistro Bliss</p>
+          </div>
+          <nav className={styles.nav}>
+            <Link href="/">
+              <Button className={styles.navItem}>Home</Button>
+            </Link>
+            <Link href="/about">
+              {' '}
+              <Button className={styles.navItem}>About</Button>
+            </Link>
+            <Link href="/menu">
+              <Button className={styles.navItem}>Menu</Button>
+            </Link>
+          </nav>
+          <Button>Book a Table</Button>
+        </div>
+      </div>
+    </header>
+  );
+}
