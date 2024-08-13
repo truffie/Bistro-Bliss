56065f4ec9a0f313f8d095a790e9083044f293f1
Truffie <trufanov.vl2013@gmail.com>
2024-08-13 20:08:21 +0300

    feat: init basket page

---
+++ src/app/basket/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { Header } from '@layout/Header/Header';
+import { FC } from 'react';
+
+const Basket: FC = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h2>Basket</h2>
+      </main>
+    </>
+  );
+};
+
+export default Basket;
