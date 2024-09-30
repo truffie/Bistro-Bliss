139b981bce9353976e44e7134251439bda2c8cfc
Truffie <trufanov.vl2013@gmail.com>
2024-09-30 14:12:06 +0300

    feat(addToOrder): update logic

--- src/app/menu/[item_id]/page.tsx
+++ src/app/menu/[item_id]/page.tsx
@@ -3,12 +3,15 @@
 import styles from './MenuItem.module.scss';
 import { useParams } from 'next/navigation';
 import { FC, useEffect, useState } from 'react';
-import { Header } from '@layout/Header/Header';
-import { menuItem, menuItems, orderCart } from '@storage';
-import { Footer } from '@layout/Footer/Footer';
+import { menuItem, menuItems } from '@storage';
 import AnimatedAppear from '@animation/Appearance';
 import Link from 'next/link';
 import { Button } from '@components/Button/Button';
+import { ClipLoader } from 'react-spinners';
+import { getStorageValue } from '../../../helpers/getLocalStorage';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { updateCart } from '../../slice/cartSlice';
 
 interface Isize {
   size: string;
@@ -17,6 +20,8 @@ interface Isize {
 }
 
 const MenuItem: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
   const { item_id } = useParams();
 
   const [item, setItem] = useState<menuItem>({
@@ -34,6 +39,17 @@ const MenuItem: FC = () => {
     { size: 'L', price: 3, isActive: false },
     { size: 'XL', price: 4, isActive: false },
   ]);
+
+  const [isAdded, setIsAdded] = useState<boolean>(false);
+
+  useEffect(() => {
+    //fake send to server
+    let timer = setTimeout(() => {
+      setIsAdded(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [isAdded]);
 
   useEffect(() => {
     const [currentItem] = menuItems.filter(item => item.id === +item_id);
@@ -64,7 +80,9 @@ const MenuItem: FC = () => {
 
       return {
         ...prev,
-        price: `${initPrice[0]} ${String(+prevPrice * findObj.price)}`,
+        price: `${initPrice[0]} ${String(
+          (+prevPrice * findObj.price).toFixed(2)
+        )}`,
       } as menuItem;
     });
 
@@ -77,65 +95,74 @@ const MenuItem: FC = () => {
     });
   }
 
+  function addToOrder() {
+    setIsAdded(true);
+
+    const previousCart: menuItem[] = getStorageValue('order', []);
+
+    const cart = previousCart.filter(el => el.id !== item.id);
+
+    localStorage.setItem('order', JSON.stringify([...cart, item]));
+
+    dispatch(updateCart(getStorageValue('order', []).length));
+  }
+
   return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.wrapper}>
-          <AnimatedAppear>
-            <div className={styles.imgWrapper}>
-              <div
-                className={
-                  item ? `${styles[item.img]} ${styles.img}` : `${styles.img}`
-                }
-              ></div>
-              <div className={styles.imgDescription}>
-                <p>22 / 35 / 63</p>
-                <p>{item?.description}</p>
-              </div>
+    <main className={styles.main}>
+      <div className={styles.wrapper}>
+        <AnimatedAppear>
+          <div className={styles.imgWrapper}>
+            <div
+              className={
+                item ? `${styles[item.img]} ${styles.img}` : `${styles.img}`
+              }
+            ></div>
+            <div className={styles.imgDescription}>
+              <p>22 / 35 / 63</p>
+              <p>{item?.description}</p>
             </div>
-          </AnimatedAppear>
-          <AnimatedAppear>
-            <div className={styles.content}>
-              <h1>{item && item.title}</h1>
-              <p>{item && item.description}</p>
-              <p>Size</p>
-              <div className={styles.sizeItemWrapper}>
-                {dishSizes.map(size => (
-                  <div
-                    key={size.size}
-                    id={size.size}
-                    className={`${styles.sizeItem} ${
-                      size.isActive ? `${styles.active}` : ''
-                    }`}
-                    onClick={e => calculatePosition(e)}
-                  >
-                    <p>{size.size}</p>
-                    <p>{size.price}s</p>
-                  </div>
-                ))}
-              </div>
-              <div className={styles.price}>
-                <p>Total:</p>
-                <p>{item && item.price}</p>
-              </div>
-              <Button
-                onClick={() => console.log('hello')}
-                className={styles.button}
-              >
-                Add to Order
-              </Button>
+          </div>
+        </AnimatedAppear>
+        <AnimatedAppear>
+          <div className={styles.content}>
+            <h1>{item && item.title}</h1>
+            <p>{item && item.description}</p>
+            <p>Size</p>
+            <div className={styles.sizeItemWrapper}>
+              {dishSizes.map(size => (
+                <div
+                  key={size.size}
+                  id={size.size}
+                  className={`${styles.sizeItem} ${
+                    size.isActive ? `${styles.active}` : ''
+                  }`}
+                  onClick={e => calculatePosition(e)}
+                >
+                  <p>{size.size}</p>
+                  <p>{size.price}s</p>
+                </div>
+              ))}
             </div>
-          </AnimatedAppear>
-          <AnimatedAppear delay={1}>
-            <Link href={`/menu/${current_id}`} scroll={false}>
-              <div className={styles.arrow}></div>
-            </Link>
-          </AnimatedAppear>
-        </div>
-      </main>
-      <Footer />
-    </>
+            <div className={styles.price}>
+              <p>Total:</p>
+              <p>{item && item.price}</p>
+            </div>
+            <Button className={styles.button} onClick={addToOrder}>
+              {!isAdded ? (
+                'Add to Order'
+              ) : (
+                <ClipLoader loading={isAdded} size={15} />
+              )}
+            </Button>
+          </div>
+        </AnimatedAppear>
+        <AnimatedAppear delay={1}>
+          <Link href={`/menu/${current_id}`} scroll={false}>
+            <div className={styles.arrow}></div>
+          </Link>
+        </AnimatedAppear>
+      </div>
+    </main>
   );
 };
 
