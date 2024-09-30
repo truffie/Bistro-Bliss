6f9bb2453432cdb09e0f0000ce397bfb9bc10074
Truffie <trufanov.vl2013@gmail.com>
2024-09-30 14:14:03 +0300

    feat: add StoreProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Header } from '@layout/Header/Header';
+import { Footer } from '@layout/Footer/Footer';
+import { StoreProvider } from './store/StoreProvider';
 
 export const metadata: Metadata = {
   title: 'Bistro Bliss',
@@ -12,8 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <StoreProvider >
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
