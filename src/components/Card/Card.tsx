a37ad849aac2413e6a06555c05e726894e16167e
Truffie <trufanov.vl2013@gmail.com>
2024-09-30 14:15:23 +0300

    feat: update Card component logic

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,34 +1,51 @@
+'use client';
+
 import { FC } from 'react';
 import styles from './Card.module.scss';
 import Link from 'next/link';
 
 export interface BaseCardProps {
+  id?: any;
   img: string;
   title: string;
-  description: string;
+  description?: string;
   className?: string;
   hover?: boolean;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  isSettings?: boolean;
 }
 
-export interface MenuCardProps extends BaseCardProps {
+export interface ActionCardProps extends BaseCardProps {
+  textAction: string;
+}
+
+export interface LinkCardProps extends BaseCardProps {
   textLink: string;
+  link?: string;
 }
 
 export interface MealCardProps extends BaseCardProps {
   price: string;
 }
 
-type CardProps = MenuCardProps | MealCardProps | BaseCardProps;
+type CardProps =
+  | ActionCardProps
+  | LinkCardProps
+  | MealCardProps
+  | BaseCardProps;
 
-function isMenuCard(props: CardProps): props is MenuCardProps {
-  return (props as MenuCardProps).textLink !== undefined;
+function isMenuCard(props: CardProps): props is LinkCardProps {
+  return (props as LinkCardProps).textLink !== undefined;
 }
 
 function isMealCard(props: CardProps): props is MealCardProps {
   return (props as MealCardProps).price !== undefined;
 }
 
-export const Card: FC<CardProps> = (props) => {
+function isActionCard(props: CardProps): props is ActionCardProps {
+  return (props as ActionCardProps).textAction !== undefined;
+}
+export const Card: FC<CardProps> = props => {
   const classes = [styles.card];
   if (props.className) {
     classes.push(props.className);
@@ -38,12 +55,33 @@ export const Card: FC<CardProps> = (props) => {
   }
 
   return (
-    <div className={`${styles.card} ${classes.join(' ')}`}>
-      <div className={`${props.img}`}></div>
-      {isMealCard(props) && <p className={styles.price}>{props.price}</p>}
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-      {isMenuCard(props) && <Link href="/menu">{props.textLink}</Link>}
-    </div>
+    <>
+      <div className={`${classes.join(' ')}`}>
+        <div className={`${props.img}`}></div>
+        {isMealCard(props) && <p className={styles.price}>{props.price}</p>}
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        {isMenuCard(props) && (
+          <Link href={props.link || '/menu'}>{props.textLink}</Link>
+        )}
+        {isActionCard(props) && <p>{props.textAction}</p>}
+      </div>
+      {props.isSettings ? (
+        <div className={styles.cardHoverSettings}>
+          <Link href={`/menu/${props.id}`}>
+            <div className={styles.burger}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </Link>
+          <div
+            className={styles.remove}
+            onClick={props.onClick}
+            id={props.id}
+          ></div>
+        </div>
+      ) : null}
+    </>
   );
 };
